5f4002638b6a14968d56390d0893d24f6062b640
sadeqbeyki <mardibaki@Gmail.com>
2025-09-14 09:59:17 +0330

    edit taskItem implemented

---
+++ TaskFlow.Web/Pages/TaskItems/Edit.cshtml.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Core;
+using TaskFlow.Infrastructure;
+
+namespace TaskFlow.Web.Pages.TaskItems
+{
+    public class EditModel : PageModel
+    {
+        private readonly TaskFlowDbContext _context;
+
+
+        public EditModel(TaskFlowDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [BindProperty]
+        public TaskItem TaskItem { get; set; } = new();
+
+
+        public async Task<IActionResult> OnGetAsync(Guid id)
+        {
+            TaskItem = await _context.TaskItems.FindAsync(id) ?? new TaskItem();
+            if (TaskItem.Id == Guid.Empty)
+                return NotFound();
+
+
+            return Page();
+        }
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+
+            _context.Attach(TaskItem).State = EntityState.Modified;
+            TaskItem.UpdatedAt = DateTime.UtcNow;
+
+
+            await _context.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
