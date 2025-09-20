cb6fa7f17b7049e6cb3088e96408c948cd2dec29
sadeqbeyki <mardibaki@Gmail.com>
2025-09-20 10:02:07 +0330

    delete task item implemented

---
+++ TaskFlow.Web/Pages/TaskItems/Delete.cshtml.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TaskFlow.Core;
+using TaskFlow.Infrastructure;
+
+namespace TaskFlow.Web.Pages.TaskItems
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly TaskFlowDbContext _context;
+
+
+        public DeleteModel(TaskFlowDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [BindProperty]
+        public TaskItem TaskItem { get; set; } = new();
+
+
+        public async Task<IActionResult> OnGetAsync(Guid id)
+        {
+            TaskItem = await _context.TaskItems.FindAsync(id) ?? new TaskItem();
+            if (TaskItem.Id == Guid.Empty)
+                return NotFound();
+
+
+            return Page();
+        }
+
+
+        public async Task<IActionResult> OnPostAsync(Guid id)
+        {
+            var task = await _context.TaskItems.FindAsync(id);
+            if (task != null)
+            {
+                _context.TaskItems.Remove(task);
+                await _context.SaveChangesAsync();
+            }
+
+
+            return RedirectToPage("Index");
+        }
+    }
+}
