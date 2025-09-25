767593c44a95c51fd690487f2f3539f962daff3f
sadeqbeyki <mardibaki@Gmail.com>
2025-09-25 10:03:30 +0330

    details item implemented

---
+++ TaskFlow.Web/Pages/TaskItems/Details.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Core;
+using TaskFlow.Infrastructure;
+
+namespace TaskFlow.Web.Pages.TaskItems
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly TaskFlowDbContext _context;
+
+
+        public DetailsModel(TaskFlowDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public TaskItem TaskItem { get; set; } = new();
+
+
+        public async Task<IActionResult> OnGetAsync(Guid id)
+        {
+            TaskItem = await _context.TaskItems.Include(t => t.Project).FirstOrDefaultAsync(t => t.Id == id) ?? new TaskItem();
+            if (TaskItem.Id == Guid.Empty)
+                return NotFound();
+
+
+            return Page();
+        }
+    }
+}
