a978b3b06ad1def551eb5abc521db88e9e412f45
sadeqbeyki <mardibaki@Gmail.com>
2025-10-27 09:54:43 +0330

    dbcontext updated, migration added

---
+++ TaskFlow.Infrastructure/Migrations/20251114062015_AddTaskItemIndexes.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskItemIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Projects",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Projects",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Projects",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
--- TaskFlow.Infrastructure/TaskFlowDbContext.cs
+++ TaskFlow.Infrastructure/TaskFlowDbContext.cs
@@ -6,31 +6,89 @@ using System.Text;
 using System.Threading.Tasks;
 using TaskFlow.Core;
 
-namespace TaskFlow.Infrastructure
+namespace TaskFlow.Infrastructure;
+
+public class TaskFlowDbContext : DbContext
 {
-    public class TaskFlowDbContext : DbContext
+    public TaskFlowDbContext(DbContextOptions<TaskFlowDbContext> options) : base(options) { }
+
+
+    //public DbSet<User> Users { get; set; } = null!;
+    //public DbSet<Project> Projects { get; set; } = null!;
+    //public DbSet<TaskItem> TaskItems { get; set; } = null!;
+
+    public DbSet<Project> Projects => Set<Project>();
+    public DbSet<TaskItem> TaskItems => Set<TaskItem>();
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public TaskFlowDbContext(DbContextOptions<TaskFlowDbContext> options) : base(options) { }
+        base.OnModelCreating(modelBuilder);
 
-
-        public DbSet<User> Users { get; set; } = null!;
-        public DbSet<Project> Projects { get; set; } = null!;
-        public DbSet<TaskItem> TaskItems { get; set; } = null!;
-
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // --- User configuration
+        modelBuilder.Entity<User>(entity =>
         {
-            base.OnModelCreating(modelBuilder);
-
+            entity.ToTable("Users");
+            entity.HasKey(u => u.Id);
 
             // Example: configure relations & constraints
-            modelBuilder.Entity<User>().HasMany(u => u.Projects).WithOne(p => p.Owner).HasForeignKey(p => p.OwnerId).OnDelete(DeleteBehavior.Cascade);
-            modelBuilder.Entity<Project>().HasMany(p => p.Tasks).WithOne(t => t.Project).HasForeignKey(t => t.ProjectId).OnDelete(DeleteBehavior.Cascade);
+            entity.HasMany(u => u.Projects)
+            .WithOne(p => p.Owner)
+            .HasForeignKey(p => p.OwnerId)
+            .OnDelete(DeleteBehavior.Cascade); //Cascade: delete Project and TaskItems //Restrict: Prevent the deletion of a PROJECT with a TASK.
 
+        });
+
+        // --- Project configuration ---
+        modelBuilder.Entity<Project>(entity =>
+        {
+            entity.ToTable("Projects");
+
+            entity.HasKey(p => p.Id);
+
+            entity.Property(p => p.Title)
+                  .IsRequired()
+                  .HasMaxLength(100);
+
+            entity.Property(p => p.Description)
+                  .HasMaxLength(500);
+
+            entity.HasMany(p => p.Tasks)
+                  .WithOne(t => t.Project)
+                  .HasForeignKey(t => t.ProjectId)
+                  .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        // --- TaskItem configuration ---
+        modelBuilder.Entity<TaskItem>(entity =>
+        {
+            entity.ToTable("TaskItems");
+
+            entity.HasKey(t => t.Id);
+
+            entity.Property(t => t.Title)
+                  .IsRequired()
+                  .HasMaxLength(100);
+
+            entity.Property(t => t.Description)
+                  .HasMaxLength(500);
+
+            entity.Property(t => t.Priority)
+                  .HasConversion<int>();
+
+            entity.Property(t => t.Status)
+                  .HasConversion<int>();
+
+            //Set CreatedAt to automatic
+            entity.Property(t => t.CreatedAt)
+                  .HasDefaultValueSql("GETUTCDATE()");
 
             // Indexes
-            modelBuilder.Entity<TaskItem>().HasIndex(t => t.Status);
-            modelBuilder.Entity<TaskItem>().HasIndex(t => t.Priority);
-        }
+            entity.HasIndex(t => t.Status);
+            entity.HasIndex(t => t.Priority);
+        });
+
+
+
     }
 }
