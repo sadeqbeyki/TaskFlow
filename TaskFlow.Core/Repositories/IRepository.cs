a03817994fdee22cbd73a6d5e1c276a0994b3ea2
sadeqbeyki <mardibaki@Gmail.com>
2025-11-17 11:50:45 +0330

    IRepository implemented (this is generic one)

---
+++ TaskFlow.Core/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskFlow.Core.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        IQueryable<T> Query();
+        Task AddAsync(T entity);
+        void Remove(T entity);
+    }
+}
