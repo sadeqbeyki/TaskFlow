b6e24b4fb0ab0c9e5da7fe934189a746987d161c
sadeqbeyki <mardibaki@Gmail.com>
2025-12-01 11:15:19 +0330

    UI - add sorting arrow in task items

--- TaskFlow.Core/Filters/TaskItemFilters.cs
+++ TaskFlow.Core/Filters/TaskItemFilters.cs
@@ -21,7 +21,7 @@ public class TaskItemFilter
 
     // paging
     public int PageNumber { get; set; } = 1;
-    public int PageSize { get; set; } = 20;
+    public int PageSize { get; set; } = 7;
 
     // sorting
     public string? SortBy { get; set; }
