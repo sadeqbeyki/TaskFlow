ff5ead90090bc31252011d83fd43b7441a9ccb5e
sadeqbeyki <mardibaki@Gmail.com>
2025-12-19 16:38:32 +0330

    ProjectTitleTests implemented

---
+++ TaskFlow.Core.Tests/ValueObjects/ProjectTitleTests.cs
@@ -0,0 +1,38 @@
+﻿using TaskFlow.Core.ValueObjects;
+
+namespace TaskFlow.Core.Tests.ValueObjects;
+
+public class ProjectTitleTests
+{
+    [Fact]
+    public void Create_WithValidTitle_ShouldSucceed()
+    {
+        // Act
+        var title = new ProjectTitle("My Project");
+
+        // Assert
+        Assert.Equal("My Project", title.Value);
+    }
+
+    [Fact]
+    public void Create_WithWhitespace_ShouldThrowException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            new ProjectTitle("   "));
+    }
+
+    [Fact]
+    public void Create_WithEmptyString_ShouldThrowException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            new ProjectTitle(string.Empty));
+    }
+
+    [Fact]
+    public void Create_ShouldTrimValue()
+    {
+        var title = new ProjectTitle("  Clean Architecture  ");
+
+        Assert.Equal("Clean Architecture", title.Value);
+    }
+}
