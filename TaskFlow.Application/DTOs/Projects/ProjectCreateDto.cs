15a11ac8452ff17f07f0f08e579e213bb443ee32
sadeqbeyki <mardibaki@Gmail.com>
2025-12-19 16:37:33 +0330

    data annotation moved to dtos

--- TaskFlow.Application/DTOs/Projects/ProjectCreateDto.cs
+++ TaskFlow.Application/DTOs/Projects/ProjectCreateDto.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace TaskFlow.Application.DTOs.Projects;
 
 public class ProjectCreateDto
 {
+    [Required(ErrorMessage = "Project title is required.")]
+    [StringLength(100, ErrorMessage = "Title must not exceed 100 characters.")]
     public string Title { get; set; } = default!;
+
+    [StringLength(500, ErrorMessage = "Description must not exceed 500 characters.")]
     public string? Description { get; set; }
 }
--- TaskFlow.Application/DTOs/Projects/ProjectUpdateDto.cs
+++ TaskFlow.Application/DTOs/Projects/ProjectUpdateDto.cs
@@ -1,7 +1,12 @@
-﻿namespace TaskFlow.Application.DTOs.Projects;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskFlow.Application.DTOs.Projects;
 
 public class ProjectUpdateDto
 {
+    [Required(ErrorMessage = "Project title is required.")]
+    [StringLength(100, ErrorMessage = "Title must not exceed 100 characters.")]
     public string Title { get; set; } = default!;
+    [StringLength(500, ErrorMessage = "Description must not exceed 500 characters.")]
     public string? Description { get; set; }
 }
